7a11db981ab819f0b26aa758e1b90ad40736bd03
Shaif Azad <shaif.rahi@northsouth.edu>
2022-01-09 00:55:55 +0600

    asset build files added from v7, driver login routing fixed

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,8 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Artisan;
 use App\Http\Controllers\Admin\ShippingChargeController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\Driver\AuthController as DriverAuthController;
+use App\Http\Controllers\Driver\DashboardController as DriverDashboardController;
 //To clear all cache
 Route::get('clear', function () {
     Artisan::call('cache:clear');
@@ -385,13 +387,13 @@ Route::group(['middleware' => 'auth:user', 'namespace' => 'User'], function () {
     Route::get('prepare-shipment-details/{id}', 'ShipmentController@PrepareShipmentEdit')->name('PrepareShipmentEdit');
 });
 
-Route::get('driver/login', 'Driver\AuthController@index');
-Route::post('driver/login', 'Driver\AuthController@login')->name('driver.login');
+Route::get('driver/login', [DriverAuthController::class,'index']);
+Route::post('driver/login', [DriverAuthController::class,'login'])->name('driver.login');
 Route::post('driver/register', 'Driver\AuthController@store')->name('driver.register');
 Route::post('driver/logout', 'Driver\AuthController@logout')->name('driver.logout');
 
 Route::group(['middleware' => 'auth:driver', 'namespace' => 'Driver', 'prefix' => 'driver'], function () {
-    Route::get('/', 'DashboardController@index')->name('driver.dashboard');
+    Route::get('/', [DriverDashboardController::class,'index'])->name('driver.dashboard');
     Route::get('/get-shipments/{type}', 'DashboardController@shipments')->name('get-driver-shipments');
     Route::get('/get-shipments-with-dates/{dates}/{type}', 'DashboardController@shipments_dates')->name('dateWize-driver-shipments');
 
