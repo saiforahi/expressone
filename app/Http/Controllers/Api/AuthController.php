3d76b5d21a7806f12d55d3bb45777a830444d710
saiforahi <shaif.rahi@northsouth.edu>
2022-01-08 16:28:50 +0600

    registration API modified

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Validator;
 use App\Models\User;
+use App\Models\Driver;
 use Auth;
 
 class AuthController extends Controller
@@ -51,22 +52,50 @@ class AuthController extends Controller
         }
         return response()->json(['success'=>false,'message' => 'Wrong credentials'], 401);
     }
-
+    public function createMerchant(array $data){
+        try{
+            $user = User::create(array_merge($data,['user_id' => 'UR'.rand(100, 999).time(),'password' => bcrypt($data['password'])]));
+            return $user;
+        }
+        catch(Exception $e){
+            return $e;
+        }
+    }
+    public function createDriver(array $data){
+        try{
+            $user = Driver::create(array_merge($data,['driver_id' => rand(),'password' => bcrypt($data['password'])]));
+            return $user;
+        }
+        catch(Exception $e){
+            return $e;
+        }
+    }
     public function register(Request $request) {
         $validator = Validator::make($request->all(), [
-            'name' => 'required|string|between:2,100',
-            'email' => 'required|string|email|max:100|unique:users',
-            "phone" => 'sometimes|nullable|string|max:11|min:11|unique:users,phone',
+            'first_name' => 'required|string|between:2,100',
+            'last_name' => 'required|string|between:2,100',
+            'email' => 'required|string|email|max:100|unique:users,email|unique:drivers,email|unique:admins,email',
+            "phone" => 'required|string|max:11|min:11|unique:users,phone|unique:drivers,phone|unique:admins,phone',
             'password' => 'required|string|min:6',
+            'shop_name' => 'sometimes|nullable',
+            'address' => 'sometimes|nullable',
+            'type'=> 'required|string'
         ]);
         if($validator->fails()){
-            return response()->json($validator->errors()->toJson(), 400);
+            return response()->json(['errors'=>$validator->errors()], 400);
         }
+        
         try{
-            $user = User::create(array_merge($validator->validated(),['password' => bcrypt($request->password)]));
+            $response='';
+            switch($request->type){
+                case 'merchant':
+                    $response=$this->createMerchant($request->all());
+                case 'driver':
+                    $response=$this->createDriver($request->except('shop_name','address'));
+            }
             return response()->json([
                 'success' => true,
-                'message' => 'User successfully registered',
+                'data' => $response,
             ], 201);
         }
         catch (Exception $e){
