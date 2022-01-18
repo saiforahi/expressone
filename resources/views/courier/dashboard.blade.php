e264958b7ce8c003bdcced1b41e17d3c03028314
Muhammad Hannan <mdhannan.info@gmail.com>
2022-01-18 20:31:06 +0600

    Done till delivery parcels in courier panel

--- resources/views/courier/dashboard.blade.php
+++ resources/views/courier/dashboard.blade.php
@@ -211,12 +211,10 @@
                     },
                 });
             });
-
             function replaceAll(str, find, replace) {
                 var escapedFind = find.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1");
                 return str.replace(new RegExp(escapedFind, 'g'), replace);
             }
         })
     </script>
-
 @endpush
